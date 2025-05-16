476cbb40adf3e1f438fc525393e7d01f969c56a9
harry chem 502 <153475377+illiteratewaffle@users.noreply.github.com>
2025-05-16 16:34:56 -0600

    connect python to mcc daq

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello wrld")
+from mcculw import ul
+from mcculw.enums import ULRange
+from mcculw.ul import ULError
+
+board_num = 0
+channel = 0
+ai_range = ULRange.BIP5VOLTS
+
+while(1 == 1):
+
+    try:
+        # Get a value from the device
+        value = ul.a_in(board_num, channel, ai_range)
+        # Convert the raw value to engineering units
+        eng_units_value = ul.to_eng_units(board_num, ai_range, value)
+
+        # Display the raw value
+        print("Raw Value: " + str(value))
+        # Display the engineering value
+        print("Engineering Value: " + '{:.4f}'.format(eng_units_value))
+    except ULError as e:
+        # Display the error
+        print("A UL error occurred. Code: " + str(e.errorcode)
+              + " Message: " + e.message)
