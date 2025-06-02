32a50ff07d78cf1912a05aed3c14ec22cd40d450
harry chem 502 <153475377+illiteratewaffle@users.noreply.github.com>
2025-06-02 14:08:14 -0600

    delete redundancies

--- main.py
+++ main.py
@@ -3,8 +3,3 @@ from Display import main as run_gui
 if __name__ == "__main__":
     # Launch the Tkinter interface
     run_gui()
-
-    # from DataAcquisition import DataAcquisition
-    # daq = DataAcquisition()
-    # daq.operatorInitials = "HH"
-    # daq.mainLoop()
