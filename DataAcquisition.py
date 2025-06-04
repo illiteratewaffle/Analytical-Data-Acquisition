8c2eb05d1ae4b53a59a13fae94c1bcb2ac723aac
harry chem 502 <153475377+illiteratewaffle@users.noreply.github.com>
2025-06-04 10:20:48 -0600

    testing 10000hz sampling rate

--- DataAcquisition.py
+++ DataAcquisition.py
@@ -1,19 +1,31 @@
+from __future__ import annotations
+
 from mcculw import ul
-from mcculw.enums import ULRange
+from mcculw.enums import ULRange, ScanOptions, FunctionType
 from mcculw.ul import ULError
 from mcculw.device_info import DaqDeviceInfo
 
 import time
 from datetime import datetime
+from ctypes import cast, POINTER, c_ushort
+from typing import List, Tuple
+
+__all__ = ["DataAcquisition"]
 
 class DataAcquisition:
-    def __init__(self):
+
+    TIME_FORMAT = "%y%m%d_%H%M%S"
+
+    def __init__(self) -> None:
         # board number: found in InstaCal
         self.board_num = 0
 
         # channel/range: see ports in MCCDAQ manual
         self.channel = 0
         self.ai_range = ULRange.BIP20VOLTS  # "ai" = analog input
+
+        # sampling rate
+        self.sample_rate = 10000 #Hz
 
         self.data = []
         self.startTime = time.perf_counter()
@@ -35,13 +47,9 @@ class DataAcquisition:
 
     def getSignalData(self) -> (float | None):
         try:
-            # GETS SIGNAL VALUE (in voltage)
-            # Get a value from the device
-            value = ul.a_in(self.board_num, self.channel, self.ai_range)
-            # Convert the raw value to normal units
-            units_value = ul.to_eng_units(self.board_num, self.ai_range, value)
-
-            return units_value
+            raw = ul.a_in(self.board_num, self.channel, self.ai_range)
+            processed = ul.to_eng_units(self.board_num, self.ai_range, raw)
+            return processed
 
         except ULError as e:  # Display the error (if needed)
             print("A UL error occurred. Code: " + str(e.errorcode) + " Message: " + e.message)
@@ -57,7 +65,7 @@ class DataAcquisition:
     def recordData(self, time: float, signal: float) -> None:
         self.data.append([time, signal])
 
-    def writeData(self, data: list) -> None:
+    def writeData(self, data: list = None) -> None:
 
         fullFilename = self.operatorInitials.upper() + "_" + self.getSystemTime() # no file extension name
         dataFile = open(fullFilename, "w")
